14a38c9aadbd24d533c5b99c55ecb0a28a00aabe
Saurabh Thakur <thakursaurabh1998@gmail.com>
2018-12-21 00:40:11 +0530

    added tests for server of generating certificates endpoint

--- test/server.test.js
+++ test/server.test.js
@@ -3,6 +3,14 @@ const request = require("supertest");
 
 const { app } = require("../server");
 
+const obj = {
+  candidateName: "Saurabh Thakur",
+  courseName: "Full Stack Nanodegree",
+  orgName: "Udacity",
+  assignDate: 1545316652295,
+  duration: 2
+};
+
 describe("GET /getAccounts", () => {
   it("should get all accounts", done => {
     request(app)
@@ -10,6 +18,32 @@ describe("GET /getAccounts", () => {
       .expect(200)
       .expect(res => {
         expect(res.body.length).toBe(10);
+      })
+      .end(done);
+  });
+});
+
+describe("POST /certificate/generate", () => {
+  it("should save the data in the blockchain", done => {
+    request(app)
+      .post("/certificate/generate")
+      .send(obj)
+      .expect(201)
+      .expect(res => {
+        expect(res.body.receipt).toBeDefined();
+        expect(res.body.data).toBeDefined();
+        const {
+          candidateName,
+          courseName,
+          orgName,
+          expirationDate
+        } = res.body.data;
+        expect(candidateName).toEqual(obj.candidateName);
+        expect(courseName).toEqual(obj.courseName);
+        expect(orgName).toEqual(obj.orgName);
+        const tempDate = new Date(obj.assignDate);
+        tempDate.setFullYear(tempDate.getFullYear() + obj.duration);
+        expect(expirationDate).toEqual(tempDate.getTime().toString());
       })
       .end(done);
   });
@@ -23,6 +57,35 @@ describe("GET /certificate/data/:id", () => {
       .expect(400)
       .expect(res => {
         expect(Object.keys(res.body)[0]).toEqual("err");
+      })
+      .end(done);
+  });
+
+  it("should get correct data for already registered candidate", done => {
+    let certificateId = "";
+    request(app)
+      .post("/certificate/generate")
+      .send(obj)
+      .expect(res => {
+        certificateId = res.body.data.certificateId;
+        request(app)
+          .get(`/certificate/data/${certificateId}`)
+          .expect(200)
+          .expect(res => {
+            const {
+              candidateName,
+              courseName,
+              orgName,
+              expirationDate
+            } = res.body;
+            expect(Object.keys(res.body).length).toBe(4);
+            expect(candidateName).toEqual(obj.candidateName);
+            expect(courseName).toEqual(obj.courseName);
+            expect(orgName).toEqual(obj.orgName);
+            const tempDate = new Date(obj.assignDate);
+            tempDate.setFullYear(tempDate.getFullYear() + obj.duration);
+            expect(expirationDate).toEqual(tempDate.getTime().toString());
+          });
       })
       .end(done);
   });
