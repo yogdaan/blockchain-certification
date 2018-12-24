63b3b8fb1a7d44d81f228e676a2c54c78ba1834b
Saurabh Thakur <thakursaurabh1998@gmail.com>
2018-12-24 16:32:02 +0530

    Loader component added

---
+++ client/src/Components/Loader.js
@@ -0,0 +1,33 @@
+import React from "react";
+import PropTypes from "prop-types";
+import { withStyles } from "@material-ui/core/styles";
+import CircularProgress from "@material-ui/core/CircularProgress";
+import Grid from "@material-ui/core/Grid";
+
+const styles = theme => ({
+  progressContainer: {
+    height: "91.5vh",
+    display: "flex",
+    justifyContent: "center",
+    alignItems: "center"
+  }
+});
+
+function CircularIndeterminate(props) {
+  const { classes } = props;
+  return (
+    <Grid item xs={12} sm={12} className={classes.progressContainer}>
+      <CircularProgress
+        size={170}
+        className={classes.progress}
+        color="secondary"
+      />
+    </Grid>
+  );
+}
+
+CircularIndeterminate.propTypes = {
+  classes: PropTypes.object.isRequired
+};
+
+export default withStyles(styles)(CircularIndeterminate);
